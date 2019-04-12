45ee6c81ef2249e806f5e351975db885b2e6999a
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-12 19:34:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,6 +64,7 @@ def turn
   puts "Please choose a number 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
+ 
   if valid_move?(index)
     player_token = current_player
     move(index, player_token)
